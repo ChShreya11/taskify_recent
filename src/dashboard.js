94f00218cbd5d8cd26b4b81a13033f95d48a5eda
ChShreya11 <98463362+ChShreya11@users.noreply.github.com>
2024-02-22 11:23:40 +0530

    Add files via upload

---
+++ src/dashboard.js
@@ -0,0 +1,171 @@
+//=============== ACTUAL DASHBOARD CODE ===============
+
+const todaysTasksContainer = document.querySelector(".Task");
+const nextDaysTasksContainer = document.querySelector(".nextDayTasksContainer");
+const thisWeeksTasksContainer = document.querySelector(
+  ".thisWeeksTasksContainer"
+);
+const dashboardBox = document.querySelector(".dashboard-box");
+const detailsBox = document.querySelector(".details-box");
+const detailsBtn = document.querySelector(".details-btn");
+
+// Helper functions
+const remDays = (currentDay) => 7 - currentDay;
+const resetTimeForDates = (startDate, testDate, endDate) => {
+  startDate.setHours(0, 0, 0, 0);
+  testDate.setHours(0, 0, 0, 0);
+  endDate.setHours(0, 0, 0, 0);
+};
+
+// Main code
+
+const groupTasks = (tasksArray) => {
+  const todayTasks = tasksArray.filter((task) => {
+    const startDate = new Date(task.startDate);
+    const endDate = new Date(task.endDate);
+    const currentDate = new Date();
+    resetTimeForDates(startDate, currentDate, endDate);
+
+    return startDate <= currentDate && currentDate <= endDate;
+  });
+
+  const nextDayTasks = tasksArray.filter((task) => {
+    const startDate = new Date(task.startDate);
+    const endDate = new Date(task.endDate);
+    const tomorrowDate = new Date();
+    tomorrowDate.setDate(tomorrowDate.getDate() + 1);
+    resetTimeForDates(startDate, tomorrowDate, endDate);
+
+    return startDate <= tomorrowDate && tomorrowDate <= endDate;
+  });
+
+  const thisWeekTasks = tasksArray.filter((task) => {
+    const startDate = new Date(task.startDate);
+    const endDate = new Date(task.endDate);
+    const thisWeekCurrentDate = new Date();
+    const thisWeekEndDate = new Date();
+    thisWeekEndDate.setDate(
+      thisWeekEndDate.getDate() + remDays(thisWeekEndDate.getDay()) - 1
+    );
+
+    resetTimeForDates(startDate, thisWeekCurrentDate, endDate);
+    resetTimeForDates(startDate, thisWeekEndDate, endDate);
+
+    while (thisWeekCurrentDate <= thisWeekEndDate) {
+      if (startDate <= thisWeekCurrentDate && thisWeekCurrentDate <= endDate) {
+        return true;
+      }
+      thisWeekCurrentDate.setDate(thisWeekCurrentDate.getDate() + 1);
+    }
+
+    return false;
+  });
+
+  return [todayTasks, nextDayTasks, thisWeekTasks];
+};
+
+const updateDashboard = () => {
+  const tasksArray = JSON.parse(localStorage.getItem("tasks"));
+  console.log(tasksArray);
+  const [todayTasks, nextDayTasks, thisWeekTasks] = groupTasks(tasksArray);
+  console.log(`todayTasks: ${todayTasks}`);
+
+  todaysTasksContainer.innerHTML = "";
+  nextDaysTasksContainer.innerHTML = "";
+  thisWeeksTasksContainer.innerHTML = "";
+
+  if (todayTasks.length === 0) {
+    todaysTasksContainer.insertAdjacentHTML(
+      "beforeend",
+      "<h2>No Tasks Available for today</h2>"
+    );
+  } else {
+    const temp = 0;
+    todayTasks.forEach((task) => {
+      const { taskName, subTask, startDate, customCategory } = task;
+      todaysTasksContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="Task-name">
+            <div>
+              <div class="task-brief">
+                <i class='bx bx-check' style='color:#39bc71; font-size: 25px;'></i>
+                <span>${taskName}</span>
+              </div>
+              <div class="details">
+                <div class="date">
+                <i class='bx bx-calendar' style='color:#118d98; font-size: 25px;'></i>
+                  <span>${startDate}</span>
+                </div>
+                <span>${subTask}</span>
+                <span>${customCategory}</span>
+              </div>
+            </div>
+            <div class="progress-container">
+              <span class="progress-details"> 25% Completed </span>
+              <progress class="progress-bar" value="25" max="100"></progress>
+            </div>
+            <button class="details-btn" type="button">Details</button>
+          </div>`
+      );
+    });
+  }
+
+  if (nextDayTasks.length === 0) {
+    nextDaysTasksContainer.insertAdjacentHTML(
+      "beforeend",
+      "<h2>No Tasks Available for tomorrow</h2>"
+    );
+  } else {
+    nextDayTasks.forEach((task) => {
+      const { taskName } = task;
+      nextDaysTasksContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="Task-name">
+        <div>
+          <div class="task-brief">
+          <i class='bx bx-check' style='color:#39bc71; font-size: 25px;'></i>
+            <span>${taskName}</span>
+          </div>
+        </div>
+
+        <button class="details-btn" type="button">Details</button>
+      </div>`
+      );
+    });
+  }
+  if (thisWeekTasks.length === 0) {
+    thisWeeksTasksContainer.insertAdjacentHTML(
+      "beforeend",
+      "<h2>No Tasks Available for this week</h2>"
+    );
+  } else {
+    thisWeekTasks.forEach((task) => {
+      const { taskName } = task;
+      thisWeeksTasksContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="Task-name">
+        <div>
+          <div class="task-brief">
+          <i class='bx bx-check' style='color:#39bc71; font-size: 25px;'></i>
+            <span>${taskName}</span>
+          </div>
+        </div>
+
+        <button class="details-btn" type="button">Details</button>
+      </div>`
+      );
+    });
+  }
+};
+
+updateDashboard();
+
+// details button click functionality
+const func = (event) => {
+  if (event.target.classList.contains("details-btn")) {
+    dashboardBox.setAttribute("hidden", true);
+    detailsBox.removeAttribute("hidden");
+  }
+};
+
+dashboardBox.addEventListener("click", func);
